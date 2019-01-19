4ffe5d1bccd73720a236212b84953ea0480e408d
unknown <diogo.haitmann@gmail.com>
2019-01-19 21:37:27 -0200

    Files prove 1

---
+++ web/Week02_Prove1/assignments.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is the landing page for Diogo Haitmann in CS313 class">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <title>Diogo R. Haitmann - Landing Page</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="scripts/modernizr-custom.js"></script>
+    </head>
+    <body>
+        <header>
+            <h1>CS313 - Assignment Portal</h1><?php echo date("m/d/Y")?>
+        </header>
+
+        <nav>
+            <!--navigation links -->
+        </nav>
+
+        <main>
+            <h2><a href="index.php">Back to Homepage</a></h2>
+            <ul>
+                <li>Assignment week 3: <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 4 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 5 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 6 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 7 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 8 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 9 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 10 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 11 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 12 <a href="#" target="_blank">Coming soon</a></li>
+                <li>Assignment week 13 <a href="#" target="_blank">Coming soon</a></li>
+            </ul>
+        </main>
+        <footer>
+            <p>&copy;2019 &bull; Diogo Haitmann &bull; SP, Brazil &bull;<a href="http://www.byui.edu/online">BYU-Idaho</a></p>
+        </footer>
+
+    </body>
+</html>
---
+++ web/Week02_Prove1/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is the landing page for Diogo Haitmann in CS313 class">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <title>Diogo R. Haitmann - Landing Page</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="scripts/modernizr-custom.js"></script>
+    </head>
+    <body>
+        <header>
+            <img src="img/Profile_image.jpg">
+            <h1>Diogo R. Haitmann </h1><?php echo date("m/d/Y")?>
+        </header>
+
+        <nav>
+            <!--navigation links -->
+        </nav>
+
+        <main>
+            <h2><a href="assignments.php">CS313 - Assignment Portal</a></h2>
+            <p>My name is Diogo Haitmann and I live in Brazil, I was born in São Paulo city, but  currently live in 1 hour far from there. I served a mission here in my country from 2006 to 2008, and I had a great time there, I learned a lot and loved serving those people and the Lord. I've been married for about 8 year, and has just 3 weeks since we discovered my wife is finally pregnant, and the blessing came in double, we'll have twins. I current work in an American company called 3M, where I play the role as Sales Intelligence specialist. I love all kind of sports and outdoor activities and every time I have a chance, I go to a new adventure.</p><br>
+            <img id="first" src="img/DUNA.JPG">
+            <img src="img/praia.jpg">
+            <img src="img/snowboard.JPG">
+            <img id="first" src="img/sky.JPG">
+            <img src="img/dive.JPG">
+            <img src="img/gun.jpg">
+
+        </main>
+        <footer>
+            <p>&copy;2019 &bull; Diogo Haitmann &bull; SP, Brazil &bull;<a href="http://www.byui.edu/online">BYU-Idaho</a></p>
+        </footer>
+
+    </body>
+</html>
